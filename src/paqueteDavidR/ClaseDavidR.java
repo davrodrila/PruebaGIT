6bcd417cb1519f3b6906431dbb58fc01359c8c72
David <davrodrila@gmail.com>
2014-05-23 17:54:06 +0200

    Paso inicial

---
+++ src/paqueteDavidR/ClaseDavidR.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteDavidR;
+
+/**
+ *
+ * @author David
+ */
+public class ClaseDavidR {
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+   
+}
