398af0430748e5dcaa3a74d5652de2e3aed8b8a3
chovigs <chovigs@gmail.com>
2014-05-31 15:12:41 +0200

    Commit con ModificadaPorAmbos solucionado

--- src/paqueteDavidR/ModificadaPorAmbos.java
+++ src/paqueteDavidR/ModificadaPorAmbos.java
@@ -1,4 +1,4 @@
-/*
+/**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,4 +14,9 @@ public class ModificadaPorAmbos {
     public void metodoDeAlumno1() {
         System.out.println("Método añadido por el alumno 1");
     }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodoAlumno2");
+    }
 }
+
