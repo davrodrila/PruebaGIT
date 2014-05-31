d20f909687ec93ddfa96d42672d931b302ce1736
David <davrodrila@gmail.com>
2014-05-31 12:42:30 +0200

    Añadida clase ModificadaPorAmbos

---
+++ src/paqueteDavidR/ModificadaPorAmbos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteDavidR;
+
+/**
+ *
+ * @author David
+ */
+public class ModificadaPorAmbos {
+    public void metodoDeAlumno1() {
+        System.out.println("Método añadido por el alumno 1");
+    }
+}
